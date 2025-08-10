a183fc0c93e35f8df62ce8c25c04d3873075e247
Mark Day <markday1962@users.noreply.github.com>
2025-08-10 06:55:25 +0100

    Update basic-tool.py

--- basic-tool-for-function-calling/basic-tool.py
+++ basic-tool-for-function-calling/basic-tool.py
@@ -4,3 +4,12 @@ load_dotenv()
 
 api_key = os.getenv("OPENAPI_API_KEY")
 
+# Initialise the LangChain LLM Client
+from langchain_openapi import ChatOpenAI
+
+llm = ChatOpenAI(
+  model="gpt-4o",
+  temperature=0.2,
+  openai_api_key=api_key
+)
+
