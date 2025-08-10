439ed98ec5c5eb3d2e2dfbade817e8b888133ca9
Mark Day <markday1962@users.noreply.github.com>
2025-08-10 07:25:14 +0100

    Update basic-tool.py

--- basic-tool-for-function-calling/basic-tool.py
+++ basic-tool-for-function-calling/basic-tool.py
@@ -1,12 +1,14 @@
 import os
+from langchain.agents import initialize_agent, AgentType
+from langchain_openapi import ChatOpenAI
+from langchain.tools import tool
+
 from dotenv import load_dotenv
 load_dotenv()
 
 api_key = os.getenv("OPENAPI_API_KEY")
 
 # Initialise the LangChain LLM Client
-from langchain_openapi import ChatOpenAI
-
 llm = ChatOpenAI(
   model="gpt-4o",
   temperature=0.2,
@@ -14,9 +16,19 @@ llm = ChatOpenAI(
 )
 
 # Basic tool for function calling
-from langchain.tools import tool
-
 @tool
 def get_exchange_rate(from_currency: str, to_currency: str) -> str:
-  """Returns a dummy exchnage rate between two currencies"""
+  """Returns a dummy exchange rate between two currencies"""
   return f"1 {from_currency} = 1.09 {to_currency}"
+
+
+def agent_executor():
+  tools = [get_exchange_rate]
+  agent = initialize.agent(tools, llm, agent=AgentType.OPENAI_FUNCTIONS)
+  response = agent.run("Convert USD to EUR")
+  print(response)
+
+
+if __name__ == "__main__":
+  agent_executor()
+  
